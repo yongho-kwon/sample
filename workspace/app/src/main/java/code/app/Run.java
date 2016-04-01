78e26afd29c8a0396bf9745193eae15c6c47da96
yongho-kwon <rnjs3980@naver.com>
2016-04-01 17:36:20 +0900

    Update Run.java

--- workspace/app/src/main/java/code/app/Run.java
+++ workspace/app/src/main/java/code/app/Run.java
@@ -9,12 +9,30 @@ public class Run {
 	private int line;
 	private double bill;
 	public Run(){
-		System.out.println("plan을 입력해주세요. silver or gold중에 선택해서 입력해주세요.");
+	System.out.println("plan을 입력해주세요. silver or gold중에 선택해서 입력해주세요.");
 		name = aScanner.next();
+		while(!(name.equals("silver") || name.equals("gold"))) {
+			System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+			name = aScanner.next();
+		}
+			
 		System.out.println("총 사용시간을 입력해주세요.");
 		minute = aScanner.nextInt();
+		
+		
+		while(!(0 <minute || 9999>minute)) {
+			System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+			minute = aScanner.nextInt();
+		}
+		
 		System.out.println("사용회선 수를 입력해주세요.");
 		line = aScanner.nextInt();
+		
+		while(!(0 <line || 99>line)) {
+			System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+			line = aScanner.nextInt();
+		}
+		
 		Calculate aCalculate = new Calculate(name,minute,line);
 		bill = aCalculate.calculate();
 		System.out.println("사용한 금액은:"+bill+"입니다.");
